b8cbb604dfe0665e13413a28ad4384fab8ece77d
Joshua McCracken <joshua@leafland.co.nz>
2021-12-14 10:23:37 +1300

    added new menu

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -9,8 +9,9 @@ let topTreesSubMenuButton = document.querySelector(
   "#top-trees-sub-menu-button"
 );
 let mainMenu = document.querySelector("#main-menu");
-let resourcesBackButton = document.querySelector("#resources-back-button");
-let topTreesBackButton = document.querySelector("#top-trees-back-button");
+let backButton = document.querySelector("#back-button");
+// let resourcesBackButton = document.querySelector("#resources-back-button");
+// let topTreesBackButton = document.querySelector("#top-trees-back-button");
 
 openMenu.addEventListener("click", () => {
   document.body.classList.add("menu-open");
@@ -19,30 +20,52 @@ closeMenu.addEventListener("click", () => {
   document.body.classList.remove("menu-open");
 });
 
+backButton.addEventListener("click", () => {
+  resourcesSubMenu.style.setProperty("opacity", "0");
+  resourcesSubMenu.style.setProperty("z-index", "0");
+  resourcesSubMenu.style.setProperty("display", "none");
+  topTreesSubMenu.style.setProperty("opacity", "0");
+  topTreesSubMenu.style.setProperty("z-index", "0");
+  topTreesSubMenu.style.setProperty("display", "none");
+  mainMenu.style.setProperty("display", "grid");
+  mainMenu.style.setProperty("opacity", "1");
+  mainMenu.style.setProperty("z-index", "9");
+  backButton.style.setProperty("opacity", "0");
+  backButton.style.setProperty("z-index", "0");
+});
+
 resourcesSubMenuButton.addEventListener("click", () => {
   mainMenu.style.setProperty("opacity", "0");
   mainMenu.style.setProperty("z-index", "0");
+  mainMenu.style.setProperty("display", "none");
+  resourcesSubMenu.style.setProperty("display", "grid");
   resourcesSubMenu.style.setProperty("opacity", "1");
   resourcesSubMenu.style.setProperty("z-index", "9");
+  backButton.style.setProperty("opacity", "1");
+  backButton.style.setProperty("z-index", "9");
 });
 
 topTreesSubMenuButton.addEventListener("click", () => {
   mainMenu.style.setProperty("opacity", "0");
   mainMenu.style.setProperty("z-index", "0");
+  mainMenu.style.setProperty("display", "none");
+  topTreesSubMenu.style.setProperty("display", "grid");
   topTreesSubMenu.style.setProperty("opacity", "1");
   topTreesSubMenu.style.setProperty("z-index", "9");
+  backButton.style.setProperty("opacity", "1");
+  backButton.style.setProperty("z-index", "9");
 });
 
-resourcesBackButton.addEventListener("click", () => {
-  resourcesSubMenu.style.setProperty("opacity", "0");
-  resourcesSubMenu.style.setProperty("z-index", "0");
-  mainMenu.style.setProperty("opacity", "1");
-  mainMenu.style.setProperty("z-index", "9");
-});
+// resourcesBackButton.addEventListener("click", () => {
+//   resourcesSubMenu.style.setProperty("opacity", "0");
+//   resourcesSubMenu.style.setProperty("z-index", "0");
+//   mainMenu.style.setProperty("opacity", "1");
+//   mainMenu.style.setProperty("z-index", "9");
+// });
 
-topTreesBackButton.addEventListener("click", () => {
-  topTreesSubMenu.style.setProperty("opacity", "0");
-  topTreesSubMenu.style.setProperty("z-index", "0");
-  mainMenu.style.setProperty("opacity", "1");
-  mainMenu.style.setProperty("z-index", "9");
-});
+// topTreesBackButton.addEventListener("click", () => {
+//   topTreesSubMenu.style.setProperty("opacity", "0");
+//   topTreesSubMenu.style.setProperty("z-index", "0");
+//   mainMenu.style.setProperty("opacity", "1");
+//   mainMenu.style.setProperty("z-index", "9");
+// });
