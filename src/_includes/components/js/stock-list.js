1ec92d9425799bf453b0917882cae78107f7d796
Joshua McCracken <joshua@leafland.co.nz>
2022-03-01 15:20:01 +1300

    more improvements

--- src/_includes/components/js/stock-list.js
+++ src/_includes/components/js/stock-list.js
@@ -1,5 +1,5 @@
 // init document elements
-let retailSearchInput = document.querySelector(".search");
+let stockSearchInput = document.querySelector(".search");
 let nextButton = document.querySelector("#next");
 let prevButton = document.querySelector("#prev");
 let resultTotals = document.querySelector("#result-totals");
@@ -8,6 +8,11 @@ let stockDataDiv = document.querySelector("#stock-data");
 let hideOutOfStock = document.querySelector("#hide-out-of-stock");
 let filterEdible = document.querySelector("#filter-edible");
 let filterNative = document.querySelector("#filter-native");
+let filterDeciduous = document.querySelector("#filter-deciduous");
+let filterEvergreen = document.querySelector("#filter-evergreen");
+let filterSemiEvergreen = document.querySelector("#filter-semi-evergreen");
+
+let compareValue = "";
 
 let stockListType = "";
 
@@ -19,8 +24,8 @@ if (window.location.href.search("retail") === -1) {
 // init other variables
 var stockData = [];
 let filteredData = [];
-let retailStart = 0;
-let retailEnd = 24;
+let stockStart = 0;
+let stockEnd = 24;
 
 let heading = [];
 
@@ -58,25 +63,25 @@ window.addEventListener("loginUpdated", () => {
       .then((response) => response.json())
       .catch((error) => {});
 
-    await displayData(stockData, retailStart, retailEnd);
+    await displayData(stockData, stockStart, stockEnd);
 
-    retailSearchInput.removeAttribute("disabled");
-    retailSearchInput.addEventListener("input", (e) => {
+    stockSearchInput.removeAttribute("disabled");
+    stockSearchInput.addEventListener("input", (e) => {
       if (e.target.value.length <= 0) {
         if (filteredData.length > 0) {
-          displayData(filteredData, retailStart, retailEnd);
+          displayData(filteredData, stockStart, stockEnd);
         } else {
-          displayData(stockData, retailStart, retailEnd);
+          displayData(stockData, stockStart, stockEnd);
         }
       } else {
-        searchRetailData(e.target.value);
+        searchStockData(e.target.value);
       }
     });
   })();
 });
 
 // display passed data in a table
-async function displayData(dataSet, retailStart, retailEnd) {
+async function displayData(dataSet, stockStart, stockEnd) {
   if (dataSet.length < 1) {
     stockDataDiv.innerHTML = `<h2 class="message">No results found</h2>`;
     resultTotals.innerHTML = `Showing 0 to 0 of 0 results`;
@@ -87,21 +92,21 @@ async function displayData(dataSet, retailStart, retailEnd) {
 
     let table = document.createElement("table");
 
-    for (let i = retailStart; i < retailEnd + 1; i++) {
-      // if we reach the end of our dataset, stop iterating
+    for (let i = stockStart; i < stockEnd + 1; i++) {
+      // if we reach the stockEnd of our dataset, stop iterating
       if (i > dataSet.length - 1) {
         break;
       }
 
       // on the first iteration, create our heading row. otherwise, create a row with our dataset at i
-      if (i === retailStart) {
+      if (i === stockStart) {
         let row = document.createElement("tr");
         heading.forEach((item) => {
           let cell = document.createElement("th");
           cell.textContent = item;
-          row.appendChild(cell);
+          row.append(cell);
         });
-        table.appendChild(row);
+        table.append(row);
       }
 
       let row = document.createElement("tr");
@@ -113,7 +118,7 @@ async function displayData(dataSet, retailStart, retailEnd) {
 
             cell.textContent = dataSet[i][j];
 
-            row.appendChild(cell);
+            row.append(cell);
           }
         }
       } else {
@@ -122,27 +127,27 @@ async function displayData(dataSet, retailStart, retailEnd) {
 
           cell.textContent = dataSet[i][j];
 
-          row.appendChild(cell);
+          row.append(cell);
         }
       }
 
-      table.appendChild(row);
+      table.append(row);
     }
 
-    stockDataDiv.appendChild(table);
-    resultTotals.innerHTML = `Showing ${retailStart + 1} to ${
-      retailEnd > dataSet.length ? dataSet.length : retailEnd + 1
+    stockDataDiv.append(table);
+    resultTotals.innerHTML = `Showing ${stockStart + 1} to ${
+      stockEnd > dataSet.length ? dataSet.length : stockEnd + 1
     } of ${dataSet.length} results`;
   }
 
-  if (retailStart === 0) {
+  if (stockStart === 0) {
     prevButton.disabled = true;
   } else {
     prevButton.disabled = false;
   }
 
   if (dataSet.length !== 0) {
-    if (retailEnd >= dataSet.length - 1) {
+    if (stockEnd >= dataSet.length - 1) {
       nextButton.disabled = true;
     } else {
       nextButton.disabled = false;
@@ -151,7 +156,7 @@ async function displayData(dataSet, retailStart, retailEnd) {
 }
 
 // a simple search for the user to filter the results
-function searchRetailData(input) {
+function searchStockData(input) {
   if (input.length > 0) {
     if (filteredData.length > 0) {
       filteredData = filteredData.filter((data) => {
@@ -181,129 +186,276 @@ function searchRetailData(input) {
       });
     }
 
-    resetStartEnd();
-    displayData(filteredData, retailStart, retailEnd);
+    resetstockStartstockEnd();
+    displayData(filteredData, stockStart, stockEnd);
   }
 }
 
-function resetStartEnd() {
-  retailStart = 0;
-  retailEnd = 24;
+function resetstockStartstockEnd() {
+  stockStart = 0;
+  stockEnd = 24;
 }
 
 // init event listeners
 
 nextButton.addEventListener("click", () => {
-  retailStart += 25;
-  retailEnd += 25;
+  stockStart += 25;
+  stockEnd += 25;
 
   if (filteredData.length > 1) {
-    displayData(filteredData, retailStart, retailEnd);
+    displayData(filteredData, stockStart, stockEnd);
   } else {
-    displayData(stockData, retailStart, retailEnd);
+    displayData(stockData, stockStart, stockEnd);
   }
 });
 
 prevButton.addEventListener("click", () => {
-  retailStart -= 25;
-  retailEnd -= 25;
+  stockStart -= 25;
+  stockEnd -= 25;
 
-  if (retailStart < 0 || retailEnd < 0) {
-    resetStartEnd();
+  if (stockStart < 0 || stockEnd < 0) {
+    resetstockStartstockEnd();
   }
 
   if (filteredData.length > 1) {
-    displayData(filteredData, retailStart, retailEnd);
+    displayData(filteredData, stockStart, stockEnd);
   } else {
-    displayData(stockData, retailStart, retailEnd);
+    displayData(stockData, stockStart, stockEnd);
   }
 });
 
 function filterData() {
   filteredData = [];
-  if (hideOutOfStock.checked === true) {
-    if (filterNative.checked === true) {
-      stockData.forEach((data) => {
-        if (parseInt(data[8]) > 0 || parseInt(data[9]) > 0) {
-          if (data[11] !== undefined) {
-            if (data[11].toLowerCase().search("native") !== -1) {
-              filteredData.push(data);
-            }
-          }
-        }
-      });
 
-      resetStartEnd();
-      displayData(filteredData, retailStart, retailEnd);
-    } else if (filterEdible.checked === true) {
-      stockData.forEach((data) => {
-        if (parseInt(data[8]) > 0 || parseInt(data[9]) > 0) {
-          if (data[11] !== undefined) {
-            if (data[11].toLowerCase().search("edible") !== -1) {
-              filteredData.push(data);
-            }
-          }
-        }
-      });
+  let compareArray = compareValue.split(",");
+  let hideFound = false;
 
-      resetStartEnd();
-      displayData(filteredData, retailStart, retailEnd);
-    } else {
-      stockData.forEach((data) => {
-        if (parseInt(data[8]) > 0 || parseInt(data[9]) > 0) {
-          filteredData.push(data);
-        }
-      });
+  if (compareArray.includes("hide")) {
+    compareArray = compareArray.filter((item) => {
+      return item !== "hide";
+    });
+    hideFound = true;
+  }
 
-      resetStartEnd();
-      displayData(filteredData, retailStart, retailEnd);
+  compareArray.pop();
+
+  for (let j = 0; j < stockData.length; j++) {
+    for (let i = 0; i < compareArray.length; i++) {
+      if (stockData[j][11].toLowerCase().search(compareArray[i]) !== -1) {
+        if (i === compareArray.length - 1) {
+          filteredData.push(stockData[j]);
+        }
+      } else {
+        break;
+      }
     }
-  } else if (filterEdible.checked === true) {
-    stockData.forEach((data) => {
-      if (data[11] !== undefined) {
-        if (data[11].toLowerCase().search("edible") !== -1) {
-          filteredData.push(data);
-        }
-      }
-    });
+  }
 
-    resetStartEnd();
-    displayData(filteredData, retailStart, retailEnd);
-  } else if (filterNative.checked === true) {
-    stockData.forEach((data) => {
-      if (data[11] !== undefined) {
-        if (data[11].toLowerCase().search("native") !== -1) {
-          filteredData.push(data);
+  if (hideFound) {
+    if (filteredData.length > 0) {
+      filteredData = filteredData.filter((item) => {
+        if (parseInt(item[8]) > 0 || parseInt(item[9]) > 0) {
+          return true;
+        } else {
+          return false;
         }
-      }
-    });
+      });
+    } else if (compareArray.length === 0) {
+      filteredData = stockData.filter((item) => {
+        if (parseInt(item[8]) > 0 || parseInt(item[9]) > 0) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    } else {
+      filteredData = [];
+    }
+  }
 
-    resetStartEnd();
-    displayData(filteredData, retailStart, retailEnd);
+  if (filteredData.length > 0) {
+    resetstockStartstockEnd();
+    displayData(filteredData, stockStart, stockEnd);
+  } else if (compareArray.length > 0) {
+    resetstockStartstockEnd();
+    displayData(filteredData, stockStart, stockEnd);
   } else {
-    resetStartEnd();
-    displayData(stockData, retailStart, retailEnd);
+    resetstockStartstockEnd();
+    displayData(stockData, stockStart, stockEnd);
   }
 }
 
 hideOutOfStock.addEventListener("input", () => {
+  compareValue = "";
+
+  if (hideOutOfStock.checked === true) {
+    compareValue += "hide,";
+  }
+  if (filterEdible.checked === true) {
+    compareValue += "edible,";
+  }
+  if (filterNative.checked === true) {
+    compareValue += "native,";
+  }
+  if (filterDeciduous.checked === true) {
+    compareValue += "deciduous,";
+  }
+  if (filterEvergreen.checked === true) {
+    compareValue += "evergreen,";
+  }
+  if (filterSemiEvergreen.checked === true) {
+    compareValue += "semi-evergreen,";
+  }
   filterData();
 });
 
 filterEdible.addEventListener("input", () => {
+  compareValue = "";
+
   if (filterEdible.checked === true) {
     filterNative.disabled = true;
+    compareValue += "edible,";
   } else {
     filterNative.disabled = false;
   }
+
+  if (hideOutOfStock.checked === true) {
+    compareValue += "hide,";
+  }
+  if (filterNative.checked === true) {
+    compareValue += "native,";
+  }
+  if (filterDeciduous.checked === true) {
+    compareValue += "deciduous,";
+  }
+  if (filterEvergreen.checked === true) {
+    compareValue += "evergreen,";
+  }
+  if (filterSemiEvergreen.checked === true) {
+    compareValue += "semi-evergreen,";
+  }
+
   filterData();
 });
 
 filterNative.addEventListener("input", () => {
+  compareValue = "";
+
   if (filterNative.checked === true) {
     filterEdible.disabled = true;
+    compareValue += "native,";
   } else {
     filterEdible.disabled = false;
   }
+
+  if (hideOutOfStock.checked === true) {
+    compareValue += "hide,";
+  }
+  if (filterEdible.checked === true) {
+    compareValue += "edible,";
+  }
+  if (filterDeciduous.checked === true) {
+    compareValue += "deciduous,";
+  }
+  if (filterEvergreen.checked === true) {
+    compareValue += "evergreen,";
+  }
+  if (filterSemiEvergreen.checked === true) {
+    compareValue += "semi-evergreen,";
+  }
+
+  filterData();
+});
+
+filterDeciduous.addEventListener("input", () => {
+  compareValue = "";
+
+  if (filterDeciduous.checked === true) {
+    filterEvergreen.disabled = true;
+    filterSemiEvergreen.disabled = true;
+    compareValue += "deciduous,";
+  } else {
+    filterEvergreen.disabled = false;
+    filterSemiEvergreen.disabled = false;
+  }
+
+  if (hideOutOfStock.checked === true) {
+    compareValue += "hide,";
+  }
+  if (filterNative.checked === true) {
+    compareValue += "native,";
+  }
+  if (filterEdible.checked === true) {
+    compareValue += "edible,";
+  }
+  if (filterEvergreen.checked === true) {
+    compareValue += "evergreen,";
+  }
+  if (filterSemiEvergreen.checked === true) {
+    compareValue += "semi-evergreen,";
+  }
+
+  filterData();
+});
+
+filterEvergreen.addEventListener("input", () => {
+  compareValue = "";
+
+  if (filterEvergreen.checked === true) {
+    filterDeciduous.disabled = true;
+    filterSemiEvergreen.disabled = true;
+    compareValue += "evergreen,";
+  } else {
+    filterDeciduous.disabled = false;
+    filterSemiEvergreen.disabled = false;
+  }
+
+  if (hideOutOfStock.checked === true) {
+    compareValue += "hide,";
+  }
+  if (filterNative.checked === true) {
+    compareValue += "native,";
+  }
+  if (filterEdible.checked === true) {
+    compareValue += "edible,";
+  }
+  if (filterDeciduous.checked === true) {
+    compareValue += "deciduous,";
+  }
+  if (filterSemiEvergreen.checked === true) {
+    compareValue += "semi-evergreen,";
+  }
+
+  filterData();
+});
+
+filterSemiEvergreen.addEventListener("input", () => {
+  compareValue = "";
+
+  if (filterSemiEvergreen.checked === true) {
+    filterDeciduous.disabled = true;
+    filterEvergreen.disabled = true;
+    compareValue += "semi-evergreen,";
+  } else {
+    filterDeciduous.disabled = false;
+    filterEvergreen.disabled = false;
+  }
+
+  if (hideOutOfStock.checked === true) {
+    compareValue += "hide,";
+  }
+  if (filterNative.checked === true) {
+    compareValue += "native,";
+  }
+  if (filterEdible.checked === true) {
+    compareValue += "edible,";
+  }
+  if (filterDeciduous.checked === true) {
+    compareValue += "deciduous,";
+  }
+  if (filterEvergreen.checked === true) {
+    compareValue += "evergreen,";
+  }
+
   filterData();
 });
