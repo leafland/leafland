559ef332f6c03109700b7aba2e3d33604e45632f
Joshua McCracken <joshua@leafland.co.nz>
2022-07-27 10:03:31 +1200

    updated confirmed order display

--- src/_includes/components/js/main.js
+++ src/_includes/components/js/main.js
@@ -12,14 +12,21 @@ if (sessionStorage.getItem("submittedOrder") === "true") {
   sessionStorage.setItem("trees", "[]");
   window.dispatchEvent(orderSent);
 
-  document.querySelector("#top-bar").style.setProperty("display", "grid");
+  document.body.classList.add("order-sent-open");
+
+  document.querySelector("#close-order-sent").addEventListener("click", () => {
+    document.body.classList.remove("order-sent-open");
+  });
+
+  // document.querySelector("#top-bar").style.setProperty("display", "grid");
   sessionStorage.setItem("submittedOrder", "false");
 
-  setTimeout(() => {
-    document.querySelector("#top-bar").style.setProperty("display", "none");
-  }, "3000");
+  // setTimeout(() => {
+  //   document.querySelector("#top-bar").style.setProperty("display", "none");
+  // }, "3000");
 } else {
-  document.querySelector("#top-bar").style.setProperty("display", "none");
+  // document.querySelector("#top-bar").style.setProperty("display", "none");
+  document.body.classList.remove("order-sent-open");
 }
 
 if (sessionStorage.getItem("trees") === null) {
