84b978dbd45a51fc6b1b4c283387f4d23d3ab0ca
Joshua McCracken <joshua@leafland.co.nz>
2022-09-06 12:44:46 +1200

    various updates

--- src/_includes/components/js/submit-order.js
+++ src/_includes/components/js/submit-order.js
@@ -327,8 +327,8 @@ submitForm.addEventListener("submit", (event) => {
   let orderTotal = document.querySelector("#order-total");
 
   send.textContent = "Submitting...";
-  send.style.setProperty("background", "var(--button-background-hover");
-  send.style.setProperty("color", "var(--button-color-hover");
+  send.style.setProperty("background", "var(--button-background-color-hover");
+  send.style.setProperty("color", "var(--button-text-color-hover");
   send.style.setProperty("cursor", "default");
 
   const {
